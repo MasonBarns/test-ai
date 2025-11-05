1e6b2d743137a171dea9286fefa342b658c4a380
MasonBarns <clayworley9@gmail.com>
2025-11-05 23:20:30 +0000

    Clean rebuild for Koyeb free tier

--- nova-app/main.py
+++ nova-app/main.py
@@ -5,15 +5,10 @@ from fastapi.templating import Jinja2Templates
 from chat import router as chat_router
 
 app = FastAPI()
-
-# Mount static files and templates
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-
-# Include chat router
 app.include_router(chat_router)
 
-# Serve Nova's frontend
 @app.get("/", response_class=HTMLResponse)
 def serve_ui(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
